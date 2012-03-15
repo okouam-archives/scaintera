511a7c283ea4e94c7b25dbcf6e4ae2599b74e718
gorbiromaric <romaric.gorbi@0-one.net>
2012-03-15 12:33:33 +0000

    the translation

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Scaintera
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :fr and all translations from config/locales/*.rb,yml are auto loaded.
+    # The default locale is :fr  and  all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
 
